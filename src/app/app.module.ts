6d02af430b1a733186de049076afd76080abc253
Donna Ryan <donna@nerdrose.com>
2020-05-14 16:19:38 -0700

    First assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { AlertsComponent } from './alerts/alerts.component';
+import { WarningComponent } from './alerts/warning/warning.component';
+import { SuccessComponent } from './alerts/success/success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    AlertsComponent,
+    WarningComponent,
+    SuccessComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
